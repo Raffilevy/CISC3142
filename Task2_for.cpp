68b87344463aee321e13a9f40b7cad5086acadc2
Raffi Levy <68696064+Raffilevy@users.noreply.github.com>
2022-09-18 11:21:53 -0700

    Update Task2_for.cpp

--- Task2_for.cpp
+++ Task2_for.cpp
@@ -6,12 +6,13 @@ int main(){
   cin >> first;
   cin >> second;
   if(first> second){
-    for(int i = second;second<first;++second){
+    for(int i = second;i<first;++i){
       cout << i;
     }
   } else {
-    for(int j=first;first < second;++first){
+    for(int j=first;j < second;++j){
       cout << first;
     }
   } return 0;
 }
+
